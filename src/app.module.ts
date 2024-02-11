1028de79433d003c4ed603268647ded44539e1d8
sam2426 <shubhamshaw6@gmail.com>
2024-02-11 15:41:21 +0530

    updates made to run all containers properly together

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserService } from './user.service';
+import { PostService } from './post.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, PostService, PrismaService],
 })
 export class AppModule {}
