fb3c014177c8bdd912e6a2067cb0c4dd194c6606
sam2426 <shubhamshaw6@gmail.com>
2024-02-11 17:53:25 +0530

    updated package.json to remove nestjs env loader

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { UserService } from './user.service';
 import { PostService } from './post.service';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [],
   controllers: [AppController],
   providers: [AppService, UserService, PostService, PrismaService],
 })
